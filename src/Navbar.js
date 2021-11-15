4f51694348ced7e0d35bf72ade8ca308cd790d9b
Fernando Pookey <nandopookey@gmail.com>
2021-11-15 11:38:05 +0700

    1

---
+++ src/Navbar.js
@@ -0,0 +1,34 @@
+// Ada 3 jenis component
+
+import React from 'react' // Ini adalah contoh class component, cukup ketik rcc kemudian enter
+
+
+// export default function Navbar() {
+//     return (
+//         <div>
+//             <h2>Navbar</h2>
+//         </div>
+//     )
+// }
+
+
+// import React from 'react'               // ini adalah contoh function component, cukup ketik rfc kemudian enter
+
+// export default function Navbar() {
+//     return (
+//         <div>
+            
+//         </div>
+//     )
+// }
+
+// arrow function
+const Navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navbar
